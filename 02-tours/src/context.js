84842639cd0e415eae8520bf137b9a09b74f2232
Erhan <erhantezer68@gmail.com>
2023-03-29 23:48:54 +0300

    context added

---
+++ 02-tours/src/context.js
@@ -0,0 +1,16 @@
+import { createContext, useContext } from "react";
+
+const AppContext = createContext();
+
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+};
+
+export const AppContextProvider = ({ children }) => {
+    
+    return (
+            <AppContextProvider value={{}}>
+                {children}
+            </AppContextProvider>
+        )
+}
