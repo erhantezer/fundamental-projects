ee9c7a865f8906d39ef39dbbdcce579c0176dbe9
Erhan <erhantezer68@gmail.com>
2023-03-30 14:03:17 +0300

    tour jsx  updated

--- 02-tours/src/pages/Tour.jsx
+++ 02-tours/src/pages/Tour.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
-const Tour = () => {
+const Tour = ({id, image, info, name, price }) => {
+
     return (
-        <div>Tour</div>
+        <article className='single-tour'>
+            <img src={image} alt={name} />
+            <footer>
+                <div>
+                    <h4>{name}</h4>
+                    <h4 className='tour-price'>${price}</h4>
+                </div>
+                <p>
+                    {}
+                </p>
+            </footer>
+        </article>
     )
 }
 
