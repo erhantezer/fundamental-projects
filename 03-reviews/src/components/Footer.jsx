66158244d8379206390c74a6a6cb3d22546ad860
Erhan <erhantezer68@gmail.com>
2023-03-30 16:54:09 +0300

    updated footer

--- 03-reviews/src/components/Footer.jsx
+++ 03-reviews/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className='bg-danger' style={{position:"fixed",bottom:"0px", textAlign:"center", width:"100vw"}}>
-            <p className='p-3'>Copyright &copy; {new Date().getFullYear()} </p>    
+            <p className='p-3'>Copyright &copy; By TEZER Tech. {new Date().getFullYear()} </p>    
         </div>
     )
 }
