1a89d23ebf89dd4120f2f1e2af588010e75e9f60
Erhan <erhantezer68@gmail.com>
2023-04-02 15:06:01 +0300

    updated next and prev

--- 03-reviews/src/hooks/useNextPer.js
+++ 03-reviews/src/hooks/useNextPer.js
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import { useGlobalContext } from '../context';
 import useCheckNum from './useCheckNum';
 
--- 03-reviews/src/hooks/usePrevPer.js
+++ 03-reviews/src/hooks/usePrevPer.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { useGlobalContext } from '../context';
+import useCheckNum from './useCheckNum';
 
 const usePrevPer = () => {
-    const { index, setIndex, name, job, image, text } = useGlobalContext();
-    
-  return (
-    <div>usePrevPer</div>
-  )
+  const { index, setIndex, name, job, image, text } = useGlobalContext();
+  const { checkNumber } = useCheckNum()
+
+  const prevNum = () => {
+    setIndex((index) => {
+      let prev = index - 1;
+      return checkNumber(prev)
+    })
+  }
+
+  return { prevNum }
 }
 
 export default usePrevPer
