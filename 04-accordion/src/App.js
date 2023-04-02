a0160e8f797b90fc862a2d5e4a2bd9467b4c6fdf
Erhan <erhantezer68@gmail.com>
2023-04-03 01:24:52 +0300

    app js aded question pages

--- 04-accordion/src/App.js
+++ 04-accordion/src/App.js
@@ -1,10 +1,24 @@
+import { useState } from "react";
+import data from "./helper/data";
+import Question from "./pages/Question";
 
 
 function App() {
-  return (
-    <div>
+  const [questions, setQuestions] = useState(data);
 
-    </div>
+  return (
+    <main>
+      <div className="container">
+        <h3>questions and answers</h3>
+        <section className="info">
+          {questions.map((question) => {
+            return (
+              <Question key={question.id} {...question} />
+              )
+          })}
+        </section>
+      </div>
+    </main>
   );
 }
 
