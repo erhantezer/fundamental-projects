9f5e001baae912cfbee5732f912c80b69a5c219e
Erhan <erhantezer68@gmail.com>
2023-04-07 11:28:54 +0300

    updated app.js

--- 08-lorem-ipsum/src/App.js
+++ 08-lorem-ipsum/src/App.js
@@ -1,7 +1,11 @@
+import { useState } from "react";
+import data from "./helper/data"
 
 
 function App() {
-  
+  const [count, setCount] = useState(0);
+
+
   return (
     <section className="section-center">
 
