1d478852fa39d56914b26686224d4d5565a56c03
Erhan <erhantezer68@gmail.com>
2023-04-09 01:20:46 +0300

    updated project

--- 09-color-generator/src/App.js
+++ 09-color-generator/src/App.js
@@ -1,9 +1,11 @@
 
 function App() {
-  return (
-    <div>
 
-    </div>
+  return (
+    <section className="container">
+      <h3>color genrator</h3>
+      <form></form>
+    </section>
   );
 }
 
