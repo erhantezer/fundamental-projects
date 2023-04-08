396d77af581d89faf806427ff2b9da489eebc3f8
Erhan <erhantezer68@gmail.com>
2023-04-08 17:38:18 +0300

    updated lorem project

--- 08-lorem-ipsum/src/App.js
+++ 08-lorem-ipsum/src/App.js
@@ -5,11 +5,15 @@ import data from "./helper/data"
 
 function App() {
   const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
 
 
   return (
     <section className="section-center">
       <h3>tired of boring lorem ipsum?</h3>
+      <form action="">
+
+      </form>
     </section>
   );
 }
