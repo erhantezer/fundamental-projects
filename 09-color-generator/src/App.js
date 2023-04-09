c3071fc0da05c2652b345823069ac3a1e170a42b
Erhan <erhantezer68@gmail.com>
2023-04-09 11:38:40 +0300

    updated project

--- 09-color-generator/src/App.js
+++ 09-color-generator/src/App.js
@@ -1,11 +1,38 @@
+import { useState } from "react";
 
 function App() {
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState("");
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
 
   return (
-    <section className="container">
-      <h3>color genrator</h3>
-      <form></form>
-    </section>
+  <>
+      <section className="container">
+        <h3>color genrator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="color"
+            id="color"
+            placeholder="#f15025"
+            onChange={(e) => setColor(e.target.value)}
+            className=""
+          />
+          <button className="btn">
+            submit
+          </button>
+        </form>
+      </section>
+      <section>
+        
+      </section>
+  </>
+
   );
 }
 
