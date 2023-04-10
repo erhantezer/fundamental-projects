ee16f2cb594499ed12cb44996394b5e3755bae79
Erhan <erhantezer68@gmail.com>
2023-04-10 17:43:14 +0300

    updated project toastify success added

--- 09-color-generator/src/pages/SingleColor.jsx
+++ 09-color-generator/src/pages/SingleColor.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import rgbToHex from "../utils/utils";
+import { toastSuccessNotify } from "../helper/toastify";
 
 
 const SingleColor = ({rgb, weight, index, hexColor}) => {
-    const [alert, setAlert] = useState(false);
+    
 
     const bcg = rgb.join(",") //! array olana join ile virgül le ayırarak string yaptık
     // console.log(bcg)
@@ -11,28 +12,20 @@ const SingleColor = ({rgb, weight, index, hexColor}) => {
     // console.log(hexValue)
     const hex = rgbToHex(...rgb)
 
-    useEffect(() => {
-    let timeout = setTimeout(() => {
-            setAlert(false)
-        }, 3000);
-        return (
-            () => clearTimeout(timeout), console.log("will unmount") //! fonksiyon clear yapılınca çalışır component will unmount
-                
-            )
-    },[index])//! index her değiştiğinde useeffect yenien çalışır component did update 
+
 
     return (
         <article
         className={`color ${index > 10 && 'color-light'}`}
         style={{backgroundColor:`rgb(${bcg})`}}
         onClick={() => {
-            setAlert(true)
+            toastSuccessNotify("Color copied to clipboard")
             navigator.clipboard.writeText(hexValue) //! kopyalama işlemi yapar
         }}
         >
             <p className="percent-value">{weight}%</p>
             <p className="color-value">{hexValue}</p>
-            {alert && <p className='alert'>copied to clipboard</p>}
+            
         </article>
     )
 }
