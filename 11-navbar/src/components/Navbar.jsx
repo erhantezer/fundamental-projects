958e88f949e41161ae2f77ce017bdb77046b2faa
Erhan <erhantezer68@gmail.com>
2023-04-15 13:12:31 +0300

    updated navbar

--- 11-navbar/src/components/Navbar.jsx
+++ 11-navbar/src/components/Navbar.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { useState } from "react";
+import logo from "../logo.svg";
+import {FaBars} from "react-icons/fa";
 
 const Navbar = () => {
+    const [show, setShow] = useState(false);
+
     return (
-        <div>Navbar</div>
+        <nav>
+            <div className='nav-center'>
+                <div className='nav-header'>
+                    <img src={logo} className="logo" alt="logo" />
+                    <button className="nav-toggle" onClick={() => setShow(!show)}>
+                        <FaBars/>
+                    </button>
+                </div>
+            </div>
+        </nav>
     )
 }
 
