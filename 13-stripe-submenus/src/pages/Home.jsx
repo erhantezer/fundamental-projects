a881ec03b30ceb57920637cbfac96cec4e9b804a
Erhan <erhantezer68@gmail.com>
2023-04-18 00:27:11 +0300

    images and data added

---
+++ 13-stripe-submenus/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+import Hero from '../components/Hero'
+import Submenu from '../components/Submenu'
+
+const Home = () => {
+    return (
+        <>
+            <Navbar/>
+            <Sidebar/>
+            <Hero/>
+            <Submenu/>
+        </>
+    )
+}
+
+export default Home
