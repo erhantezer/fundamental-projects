223e1337b20038def2425d5310e46ad14a687c3f
Erhan <erhantezer68@gmail.com>
2023-04-19 15:47:21 +0300

    updated

--- 13-stripe-submenus/src/components/Submenu.jsx
+++ 13-stripe-submenus/src/components/Submenu.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import { useState, useEffect, useRef } from 'react';
+import { useGlobalContext } from '../context';
 
 const Submenu = () => {
+    const {submenu, page, location} = useGlobalContext();
     
     return (
         <div>Submenu</div>
