006ebbbe88f228ebf3c8012af0057ed6f36dbc80
Erhan <erhantezer68@gmail.com>
2023-04-20 16:00:31 +0300

    updated

--- 13-stripe-submenus/src/components/Submenu.jsx
+++ 13-stripe-submenus/src/components/Submenu.jsx
@@ -2,10 +2,27 @@ import { useState, useEffect, useRef } from 'react';
 import { useGlobalContext } from '../context';
 
 const Submenu = () => {
-    const {submenu, page, location} = useGlobalContext();
-    
+    const { submenu, page, location } = useGlobalContext();
+    const container = useRef();
+    const [colums, setColums] = useState('col-2')
+
     return (
-        <div>Submenu</div>
+        <aside className={`${submenu ? 'submenu show' : 'submenu'}`} ref={container}>
+            <section>
+                <h4>{page}</h4>
+                <div className={`submenu-center ${colums}`}>
+                    {links.map((link, index) => {
+                        const { url, icon, label } = link
+                        return (
+                            <a key={index} href={url}>
+                                {icon}
+                                {label}
+                            </a>
+                        )
+                    })}
+                </div>
+            </section>
+        </aside>
     )
 }
 
