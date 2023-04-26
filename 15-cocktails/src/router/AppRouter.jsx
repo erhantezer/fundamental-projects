db58bd3a01393eac37aa89aabc2fbd9c9b962d5b
Erhan <erhantezer68@gmail.com>
2023-04-26 17:49:29 +0300

    edited project and updated

--- 15-cocktails/src/router/AppRouter.jsx
+++ 15-cocktails/src/router/AppRouter.jsx
@@ -1,8 +1,24 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import PrivateRouter from "./PrivateRouter";
+import SingleCocktail from "../pages/SingleCocktail";
+import Error from "../pages/Error";
 
 const AppRouter = () => {
   return (
-    <div>AppRouter</div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="about" element={<About/>}/>
+        <Route path="cocktail/:id" element={<PrivateRouter/>}>
+          <Route path="" element={<SingleCocktail/>}/>
+        </Route>
+        <Route path="*" element={<Error/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
