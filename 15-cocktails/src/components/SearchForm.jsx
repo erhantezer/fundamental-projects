894ec6edbb4585eef9b56dea94fa96f1789db3f0
Erhan <erhantezer68@gmail.com>
2023-04-29 11:21:13 +0300

    updated searchform

--- 15-cocktails/src/components/SearchForm.jsx
+++ 15-cocktails/src/components/SearchForm.jsx
@@ -1,8 +1,34 @@
+import { useState } from "react"
 
 
 const SearchForm = () => {
+    const [search, setSearch] = useState("");
+
+    const searchCocktail = (e) => {
+        setSearch(e.target.value)
+    }
+
+const handleSubmit = (e) => {
+    e.preventDefault()
+}
+
+
     return (
-        <div>SearchForm</div>
+        <section className="section search">
+            <form className="search-form" onSubmit={handleSubmit}>
+                <label htmlFor="search">
+                    search your favorite cocktail
+                </label>
+                <input 
+                type="text" 
+                id="search"
+                name="search"
+                value={search}
+                placeholder="Enter cocktail ..."
+                onChange={searchCocktail}
+                />
+            </form>
+        </section>
     )
 }
 
