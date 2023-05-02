2326197eda11df0501b2363a08bf8f9daa030898
Erhan <erhantezer68@gmail.com>
2023-05-02 21:30:55 +0300

    updated makdown project

--- 16-markdown-preview/src/App.jsx
+++ 16-markdown-preview/src/App.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react"
 
 function App() {
-  
+ const [makdown, setMarkdown] = useState()
 
   return (
-    <>
-      
-    </>
+    <main>
+        <section className="section">
+          <textarea 
+          name="" 
+          id="" 
+          cols="30" 
+          rows="10"
+          ></textarea>
+        </section>
+    </main>
   )
 }
 
