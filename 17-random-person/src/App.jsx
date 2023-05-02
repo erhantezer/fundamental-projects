a7fae22b2f396f55b543f74cde85cdb80839a49a
Erhan <erhantezer68@gmail.com>
2023-05-02 22:10:33 +0300

    updated project app.jsx

--- 17-random-person/src/App.jsx
+++ 17-random-person/src/App.jsx
@@ -1,9 +1,22 @@
+import { useState } from 'react';
+import {
+  FaEnvelopeOpen,
+  FaUser,
+  FaCalendarTimes,
+  FaMap,
+  FaPhone,
+  FaLock,
+} from 'react-icons/fa';
+
+const url = 'https://randomuser.me/api/';
+const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg';
 
 function App() {
-  
+  const [loading, setLoading] = useState()
+
   return (
     <>
-     
+
     </>
   )
 }
