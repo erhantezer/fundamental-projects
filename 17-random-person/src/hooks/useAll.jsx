3b971a00ea26e88abd463e1fb39dcb30c12b0e43
Erhan <erhantezer68@gmail.com>
2023-05-03 22:23:10 +0300

    updated hook

--- 17-random-person/src/hooks/useAll.jsx
+++ 17-random-person/src/hooks/useAll.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react"
 
 
 const useAll = () => {
+    const [loading, setLoading] = useState(false);
+    const [person, setPerson] = useState(null);
+    const [value, setValue] = useState("random person");
+    const [title, setTitle] = useState("name");
 
 return {}
 }
