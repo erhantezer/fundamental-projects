6ec96cc98a9e419faa98b7029308008cab387a7f
Erhan <erhantezer68@gmail.com>
2023-05-06 00:09:07 +0300

    updated project

--- 18-pagination/src/hooks/useFetch.jsx
+++ 18-pagination/src/hooks/useFetch.jsx
@@ -1,12 +1,19 @@
 import { useEffect, useState } from 'react';
 
+
+const url = 'https://api.github.com/users/john-smilga/followers?per_page=100';
+
 const useFetch = () => {
     const [loading, setLoading] = useState(false);
     const [veri, setVeri] = useState([]);
 
-    const getProducts = () => {
+    const getProducts = async () => {
+        setLoading(true)
         try {
-            
+            const response = await fetch();
+            const data = await response.json();
+            setVeri(data);
+            setLoading(false);
         } catch (error) {
             
         }
@@ -14,7 +21,7 @@ const useFetch = () => {
 
     useEffect(() => {
         getProducts()
-    },[])
+    },[]);
 
     return {}
 }
