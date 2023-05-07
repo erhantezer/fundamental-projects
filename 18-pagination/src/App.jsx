45f9b24a1da6951bbf59da4ce10e11ade0b1b288
Erhan <erhantezer68@gmail.com>
2023-05-07 13:52:35 +0300

    updated jsx app.jsx

--- 18-pagination/src/App.jsx
+++ 18-pagination/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import useFetch from "./hooks/useFetch";
+import Follower from "./components/Follower";
 
 
 function App() {
@@ -7,9 +8,19 @@ function App() {
   console.log(veri)
 
   return (
-    <>
-      
-    </>
+    <main>
+      <div className="section-title">
+        <h1>{loading ? 'loading...' : 'pagination'}</h1>
+        <div className="underline"></div>
+      </div>
+      <section className="followers">
+        <div className="container">
+          {null.map((follower) => {
+            return <Follower/>
+          })}
+        </div>
+      </section>
+    </main>
   )
 }
 
