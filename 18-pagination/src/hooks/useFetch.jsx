a9d155ea4ebba7fc52e19e47aae655d6c290d05f
Erhan <erhantezer68@gmail.com>
2023-05-07 13:39:04 +0300

    updated project

--- 18-pagination/src/hooks/useFetch.jsx
+++ 18-pagination/src/hooks/useFetch.jsx
@@ -7,6 +7,23 @@ const useFetch = () => {
     const [loading, setLoading] = useState(false);
     const [veri, setVeri] = useState([]);
 
+
+    //! Sequence generator function (commonly referred to as "range", e.g. Clojure, PHP, etc.)
+    //? const range = (start, stop, step) =>
+    //?     Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step);
+
+    //! Generate numbers range 0..4
+    //? range(0, 4, 1);
+    // [0, 1, 2, 3, 4]
+
+    //! Generate numbers range 1..10 with step of 2
+    //? range(1, 10, 2);
+    // [1, 3, 5, 7, 9]
+
+
+
+
+
     const getProducts = async () => {
         setLoading(true)
         try {
@@ -39,10 +56,6 @@ const useFetch = () => {
     },[]);
 
 
-
-    
-
-    
 
 
 
