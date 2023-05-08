452f94819d926ac9d59054e2e825ad759c557645
Erhan <erhantezer68@gmail.com>
2023-05-08 21:16:21 +0300

    added next and prev and veri

--- 18-pagination/src/App.jsx
+++ 18-pagination/src/App.jsx
@@ -31,6 +31,28 @@ function App() {
             return <Follower key={follower.id} {...follower}/>
           })}
         </div>
+        {!loading && (
+          <div className="btn-container">
+              <button className="prev-btn" onClick={prevPage}>
+                prev
+              </button>
+              {veri.map((item, index) => {
+              return (
+                <button
+                key={index}
+                className={`page-btn ${index === page ? 'active-btn' : null}`}
+                onClick={() => handlePage(index)}
+                >
+                  {index + 1}
+                </button>
+              )
+              })}
+              <button className="next-btn" onClick={nextPage}>
+                next
+              </button>
+          </div>
+          
+          )}
       </section>
     </main>
   )
