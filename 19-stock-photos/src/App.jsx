2a73c89da6023bb34191bf3c80fa3b01287af978
Erhan <erhantezer68@gmail.com>
2023-05-11 23:42:36 +0300

    apikey added

--- 19-stock-photos/src/App.jsx
+++ 19-stock-photos/src/App.jsx
@@ -1,5 +1,8 @@
 
 
+const clientID = `?client_id=-WsG44wZbP_-YsLll3ho70imGuxqUSGtc2kbBajE7Bc`;
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 const App = () => {
   return (
