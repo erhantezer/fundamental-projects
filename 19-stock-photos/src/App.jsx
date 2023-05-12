7d3f340aea1478927bcd1fa2d395bd1d5b781ffc
Erhan <erhantezer68@gmail.com>
2023-05-12 14:54:53 +0300

    updated project

--- 19-stock-photos/src/App.jsx
+++ 19-stock-photos/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 
 const clientID = `?client_id=-WsG44wZbP_-YsLll3ho70imGuxqUSGtc2kbBajE7Bc`;
@@ -5,6 +6,9 @@ const mainUrl = `https://api.unsplash.com/photos/`;
 const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 const App = () => {
+  const [search, setSearch] = useState("cat");
+  const [loading, setLoading] = useState(false);
+  
   return (
     <div>App</div>
   )
