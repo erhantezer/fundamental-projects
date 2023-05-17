e47ac52c425cb9b7aa6a9146e1f0b49b7b7edb0d
Erhan <erhantezer68@gmail.com>
2023-05-17 21:47:28 +0300

    edited project

--- 19-stock-photos/src/App.jsx
+++ 19-stock-photos/src/App.jsx
@@ -1,51 +1,51 @@
 import Photo from "./Photo";
 import { useGlobalContext } from "./context";
-import {FaSearch} from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
 
 
 const App = () => {
-  const { 
-    loading, 
-    page, 
-    setPage, 
-    photos, 
-    query, 
+  const {
+    loading,
+    page,
+    setPage,
+    photos,
+    query,
     setQuery,
     dataFetch,
   } = useGlobalContext();
 
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  console.log(query)
-}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(query)
+  }
 
-if (loading) {
-  return <div style={{textAlign:"center"}}>
-    <h1 className="loading">Loading...</h1>
+  if (loading) {
+    return <div style={{ textAlign: "center" }}>
+      <h1 className="loading">Loading...</h1>
     </div>
-}
+  }
 
   return (
     <main>
       <section className="search">
         <form className="search-form" onSubmit={handleSubmit}>
-          <input 
-          type="text" 
-          value={query}
-          placeholder="search"
-          onChange={(e) => setQuery(e.target.value)}
-          className="form-input"
+          <input
+            type="text"
+            value={query}
+            placeholder="search"
+            onChange={(e) => setQuery(e.target.value)}
+            className="form-input"
           />
-          <button style={{cursor:"pointer"}} type="submit" className="submit-btn">
-            <FaSearch/>
+          <button style={{ cursor: "pointer" }} type="submit" className="submit-btn">
+            <FaSearch />
           </button>
         </form>
       </section>
       <section className="photos">
         <div className="photos-center">
           {photos?.map((photo, index) => {
-            return <Photo key={index} {...photo}/>
+            return <Photo key={index} {...photo} />
           })}
         </div>
       </section>
--- 19-stock-photos/src/Photo.jsx
+++ 19-stock-photos/src/Photo.jsx
@@ -6,11 +6,11 @@ const Photo = () => {
             <img className='photo-grid' src={null} alt={null} />
             <div className='photo-info'>
                 <div>
-                    <h4>{}</h4>
-                    <p>{} likes</p>
+                    <h4>{ }</h4>
+                    <p>{ } likes</p>
                 </div>
                 <div>
-                    <p>{}</p>
+                    <p>{ }</p>
                 </div>
                 <a href={null}>
                     <img className='user-img' src={null} alt={null} />
