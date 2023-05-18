5bb4f6a7eee076062aee43773b6f5c083e2dd5cf
Erhan <erhantezer68@gmail.com>
2023-05-18 22:29:39 +0300

    updated project app.jsx

--- 20-dark-mode/src/App.jsx
+++ 20-dark-mode/src/App.jsx
@@ -4,9 +4,19 @@ function App() {
 
 
   return (
-    <>
-      
-    </>
+    <main>
+      <nav>
+        <div className="nav-center">
+          <h1>overreacted</h1>
+          <button className="btn">
+            toggle
+          </button>
+        </div>
+      </nav>
+      <section className="articles">
+        {}
+      </section>
+    </main>
   )
 }
 
