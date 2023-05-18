5586f641d9aaec782ed4ece0fcb3328ddf52308a
Erhan <erhantezer68@gmail.com>
2023-05-18 22:48:55 +0300

    toggle added

--- 20-dark-mode/src/App.jsx
+++ 20-dark-mode/src/App.jsx
@@ -1,20 +1,29 @@
-
+import { useState } from "react";
 
 function App() {
+  const [theme, setTheme] = useState("light-theme");
 
+
+  const toggleTheme = () => {
+    if () {
+      
+    } else {
+      
+    }
+  }
 
   return (
     <main>
       <nav>
         <div className="nav-center">
           <h1>overreacted</h1>
-          <button className="btn">
+          <button className="btn" onClick={toggleTheme}>
             toggle
           </button>
         </div>
       </nav>
       <section className="articles">
-        {}
+        { }
       </section>
     </main>
   )
