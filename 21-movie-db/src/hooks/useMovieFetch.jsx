9f1e392529341a7ac2cb1ee35901a71400d1eaf7
Erhan <erhantezer68@gmail.com>
2023-05-20 23:35:32 +0300

    updated

--- 21-movie-db/src/hooks/useMovieFetch.jsx
+++ 21-movie-db/src/hooks/useMovieFetch.jsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
 
+
+
+export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
+const url ='https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png';
+
 const useMovieFetch = () => {
 const [loading, setLoading] = useState(false);
 const [error, setError] = useState(false);
