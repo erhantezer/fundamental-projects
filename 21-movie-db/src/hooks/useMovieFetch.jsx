abc4a56651794dedf25610344e55c93c1f0e3e25
Erhan <erhantezer68@gmail.com>
2023-05-20 09:36:14 +0300

    updated

--- 21-movie-db/src/hooks/useMovieFetch.jsx
+++ 21-movie-db/src/hooks/useMovieFetch.jsx
@@ -1,7 +1,9 @@
-
+import { useState } from "react";
 
 const useMovieFetch = () => {
-
+const [loading, setLoading] = useState(false);
+const [error, setError] = useState(false);
+const [movies, setMovies] = useState([]);
 
 
     return {}
