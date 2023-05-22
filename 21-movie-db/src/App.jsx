8ce52fbf882942c69f699af2637c40527993ca2e
Erhan <erhantezer68@gmail.com>
2023-05-23 00:21:31 +0300

    updated

--- 21-movie-db/src/App.jsx
+++ 21-movie-db/src/App.jsx
@@ -1,10 +1,38 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/router";
+import { useEffect, useState } from "react";
 
+
+const getStorageTheme = () => {
+  let theme = "light-theme";
+  if(localStorage.getItem("theme")){
+    theme = localStorage.getItem("theme")
+  }
+  return theme
+}
 
 function App() {
+  const [theme, setTheme] = useState(getStorageTheme());
+  
+
+  const toggleTheme = () => {
+    if (theme === "light-theme") {
+      setTheme("dark-theme")
+    } else {
+      setTheme("light-theme")
+    }
+  }
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme)
+    document.documentElement.className = theme;
+  }, [theme]);
+
   return (
     <>
+    <button className="btn" onClick={toggleTheme}>
+      Toggle
+    </button>
       <RouterProvider router={router} />
     </>
   )
