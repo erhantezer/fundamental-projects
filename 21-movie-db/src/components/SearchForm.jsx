a2c9abb0b5fcdce052975dbacc66beeaf1b102a5
Erhan <erhantezer68@gmail.com>
2023-05-22 21:13:07 +0300

    updated project

--- 21-movie-db/src/components/SearchForm.jsx
+++ 21-movie-db/src/components/SearchForm.jsx
@@ -1,8 +1,20 @@
+import useMovieFetch from "../hooks/useMovieFetch"
 
 
 const SearchForm = () => {
+    const {query, setQuery, error} = useMovieFetch()
     return (
-        <div>SearchForm</div>
+        <form className="search-form">
+            <h2>search movies</h2>
+            <input 
+            type="text" 
+            placeholder="Enter movie"
+            className="form-input"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            />
+            
+        </form>
     )
 }
 
