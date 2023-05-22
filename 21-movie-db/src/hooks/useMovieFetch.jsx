3592b3d49aabe95c49baec631d1ab36126aafa05
Erhan <erhantezer68@gmail.com>
2023-05-22 22:01:14 +0300

    project api fetch finished

--- 21-movie-db/src/hooks/useMovieFetch.jsx
+++ 21-movie-db/src/hooks/useMovieFetch.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 
 const API_ENDPOINT = `https://www.omdbapi.com/?apikey=fcacb142`;
 
-
 const useMovieFetch = () => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState({ show: false, msg: '' });
@@ -12,9 +11,9 @@ const useMovieFetch = () => {
     const fetchMovie = async () => {
         setLoading(true)
         let url;
-        if(query ) {
+        if (query) {
             url = `${API_ENDPOINT}&s=${query}`
-        }else {
+        } else {
             url = `${API_ENDPOINT}`
         }
 
@@ -22,11 +21,18 @@ const useMovieFetch = () => {
         try {
             const res = await fetch(url)
             const data = await res.json()
+            if (data.Response === "True") {
+                setMovies(data.Search || data)
+                setError({ show: false, msg: '' })
+            } else {
+                setError({ show: true, msg: data.Error })
+            }
+
             console.log(data)
             setLoading(false)
         } catch (error) {
             console.log(error)
-            setError(true)
+
         }
     }
 
