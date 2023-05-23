be4cee51ec1826b02bad743433230e052acf10c9
Erhan <erhantezer68@gmail.com>
2023-05-23 22:55:54 +0300

    updated

--- 21-movie-db/src/pages/SingleMovies.jsx
+++ 21-movie-db/src/pages/SingleMovies.jsx
@@ -8,9 +8,23 @@ const SingleMovies = () => {
     const {id} = useParams()
     const {loading, error, movies:movie} = useMovieFetch(`&i=${id}`)
     
+    if (loading) {
+        return <div className='loading'></div>
+    }
 
+    if (error.show) {
+        return (
+            <div className='page-error'>
+                <h1>{error.msg}</h1>
+                <Link to='/' className='btn'>
+                    back to movies
+                </Link>
+            </div>
+        )
+    }
 
-    const { imdbId, Poster: poster, Title: title, Year: year } = movie;
+    const { Poster: poster, Title: title, Plot: plot, Year: year } = movie;
+    
     return (
         <div>SingleMovies</div>
     )
