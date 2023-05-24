4c56f1b0b40b2c9d3db0401d5822a78860e71b98
Erhan <erhantezer68@gmail.com>
2023-05-24 23:24:54 +0300

    context structure

---
+++ 22-hacker-news/src/context.jsx
@@ -0,0 +1,18 @@
+import { createContext, useContext } from "react";
+
+
+const AppContext = createContext();
+
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+};
+
+export const AppProvider = ({ children }) => {
+    
+
+    return (
+        <AppContext.Provider>
+            {children}
+        </AppContext.Provider>
+    )
+}
