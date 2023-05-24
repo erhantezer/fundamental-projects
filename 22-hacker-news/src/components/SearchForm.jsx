22d6265d17f269b90d08383df42d86b00246e01d
Erhan <erhantezer68@gmail.com>
2023-05-24 23:36:27 +0300

    updated searchForm

--- 22-hacker-news/src/components/SearchForm.jsx
+++ 22-hacker-news/src/components/SearchForm.jsx
@@ -1,8 +1,27 @@
+import { useState } from "react"
 
 
 const SearchForm = () => {
+    const [query, setQuery] = useState("aaa");
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    const handleChange = (e) => {
+        setQuery(e.target.value)
+    }
+
     return (
-        <div>SearchForm</div>
+        <form className="search-form" onSubmit={handleSubmit}>
+            <h2>search hacker news</h2>
+            <input 
+            type="text" 
+            className="form-input"
+            value={query}
+            onChange={handleChange}
+            />
+        </form>
     )
 }
 
