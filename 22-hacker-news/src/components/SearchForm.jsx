fc9c91a382de3f056916c4d9895d7adc0ecff496
Erhan <erhantezer68@gmail.com>
2023-05-25 22:23:39 +0300

    actions added

--- 22-hacker-news/src/components/SearchForm.jsx
+++ 22-hacker-news/src/components/SearchForm.jsx
@@ -1,25 +1,25 @@
-import { useState } from "react"
+
 
 
 const SearchForm = () => {
-    const [query, setQuery] = useState("aaa");
+    
 
     const handleSubmit = (e) => {
         e.preventDefault()
     }
 
     const handleChange = (e) => {
-        setQuery(e.target.value)
+        
     }
 
     return (
         <form className="search-form" onSubmit={handleSubmit}>
             <h2>search hacker news</h2>
-            <input 
-            type="text" 
-            className="form-input"
-            value={query}
-            onChange={handleChange}
+            <input
+                type="text"
+                className="form-input"
+                value={}
+                onChange={handleChange}
             />
         </form>
     )
