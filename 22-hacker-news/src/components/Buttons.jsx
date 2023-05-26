cde6d3c8312058c3e848d27169240a2b36850aa9
Erhan <erhantezer68@gmail.com>
2023-05-26 22:54:35 +0300

    updated

--- 22-hacker-news/src/components/Buttons.jsx
+++ 22-hacker-news/src/components/Buttons.jsx
@@ -1,19 +1,22 @@
+import { useGlobalContext } from "../context"
 
 
 const Buttons = () => {
+const {isLoading, page, nbPages, handlePage} = useGlobalContext()
+
     return (
         <div className='btn-container'>
-            <button disabled={null}>
+            <button disabled={isLoading} onClick={() => handlePage('dec')}>
                 prev
             </button>
             <p>
-                
+                {page + 1} of {nbPages}
             </p>
-            <button disabled={null} >
+            <button disabled={isLoading} onClick={() => handlePage('inc')} >
                 next
             </button>
         </div>
     )
 }
 
-export default Buttons
+export default Buttons;
