d0138bf07ffee3b82c3c6b48ea1a59865cc21801
Erhan <erhantezer68@gmail.com>
2023-05-27 23:06:18 +0300

    loading added

--- 22-hacker-news/src/components/Stories.jsx
+++ 22-hacker-news/src/components/Stories.jsx
@@ -1,6 +1,13 @@
+import { useGlobalContext } from "../context"
 
 
 const Stories = () => {
+    const { isLoading, hits, removeStory } = useGlobalContext();
+
+    if (isLoading) {
+        return <div className='loading'></div>
+    }
+    
     return (
         <div>Stories</div>
     )
