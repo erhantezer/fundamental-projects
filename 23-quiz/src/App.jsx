e366790418e858b8eff158b6b82c1b0f726ffabc
Erhan <erhantezer68@gmail.com>
2023-05-29 22:32:49 +0300

    updated app.jsx

--- 23-quiz/src/App.jsx
+++ 23-quiz/src/App.jsx
@@ -1,9 +1,26 @@
+import Modal from "./components/Modal"
 
 function App() {
 
   return (
     <main>
-      
+      <Modal />
+      <section className='quiz'>
+        <p className='correct-answers'>
+          correct answers : 
+        </p>
+        <article className='container'>
+          <h2 
+          // dangerouslySetInnerHTML={{ __html:  }} 
+          />
+          <div className='btn-container'>
+            
+          </div>
+        </article>
+        <button className='next-question' >
+          next question
+        </button>
+      </section>
     </main>
   )
 }
